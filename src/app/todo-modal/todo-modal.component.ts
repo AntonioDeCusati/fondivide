9219bbcd170f01973e51e80882860044d0026c9b
antonio.decusati <antonio.decusati@progettopa.it>
2019-10-01 12:59:59 +0200

    no message

--- src/app/todo-modal/todo-modal.component.ts
+++ src/app/todo-modal/todo-modal.component.ts
@@ -3,11 +3,10 @@ import { ModalController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
 import { Observable, throwError } from 'rxjs';
 import { Fondo } from '../model/fondo.model';
-import { HttpClient, HttpResponse, HttpHeaders, HttpParams} from '@angular/common/http';
-import { RequestOptions, Headers , Http } from '@angular/http';
+import { HttpClient, HttpResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { RequestOptions, Headers, Http } from '@angular/http';
 import { catchError } from 'rxjs/operators';
 import { Movimento } from '../model/movimento.model';
-
 
 @Component({
   selector: 'app-todo-modal',
@@ -16,19 +15,21 @@ import { Movimento } from '../model/movimento.model';
 })
 export class TodoModalComponent implements OnInit {
 
-  private urlGetFondo = "http://www.antoniodecusati.it/connDb/project/fondivide/api/getAllFondi.php";
-  private urlAddAccredito = "http://www.antoniodecusati.it/connDb/project/fondivide/api/addMovimentoAccredito.php";
+  private urlGetFondo = 'http://www.antoniodecusati.it/connDb/project/fondivide/api/getAllFondi.php';
+  private urlAddAccredito = 'http://www.antoniodecusati.it/connDb/project/fondivide/api/addMovimentoAccredito.php';
   @Input() data: any;
   movimento: Movimento;
   title: string;
   content: any;
   fondo: any;
-  fondi: any;//Observable<HttpResponse<Fondo[]>>;
-  fondiMap: any;//HttpResponse<Fondo[]>;
+  fondi: any; // Observable<HttpResponse<Fondo[]>>;
+  fondiMap: any; // HttpResponse<Fondo[]>;
   itemsModal: Array<{}>;
   itemSaved = {};
+  saldoBooleanTrue = false;
+  saldoErrorClass = false;
 
-  idFondo: number;
+  idFondo: any;
   descrizione: string;
   valore: number;
   saldoDaRipartire: number;
@@ -37,7 +38,7 @@ export class TodoModalComponent implements OnInit {
   luogo: string;
   date: number;
 
-  
+
 
   constructor(private modalController: ModalController, private http: HttpClient, public toastController: ToastController) { }
 
@@ -46,13 +47,14 @@ export class TodoModalComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.data);
+    console.debug(this.data);
     this.loadFondiId();
-    if (this.data == "C") {
-      this.title = "Credito";
-      //text,number,date,toogle
+    this.refreshVisibility();
+    if (this.data === 'C') {
+      this.title = 'Credito';
+      // text,number,date,toogle
     } else {
-      this.title = "Debito"
+      this.title = 'Debito';
     }
 
   }
@@ -67,14 +69,20 @@ export class TodoModalComponent implements OnInit {
     return this.http.get(url);
   }
 
-  addCredito(movimento : Movimento): any {
-    let  headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*'});
-    return this.http.post(this.urlAddAccredito,movimento,{headers : headers,observe: "body", params : { movimento : JSON.stringify(movimento)}})
-      .subscribe(
-      result => {
-        console.log("Result : ", result);
+  addCredito(movimento: Movimento): any {
+    const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*' });
+    headers.append('Access Control Allow MethodAccess Control Allow Methods', '*');
+    return this.http.post(this.urlAddAccredito, movimento, {
+      headers, params: { movimento: JSON.stringify(movimento) },
+      responseType: 'json'
+    }).subscribe(
+      data => {
+        console.debug('POST Request is successful ', data);
+      },
+      error => {
+        console.debug('Error POST : ', error);
       }
-      );
+    );
   }
 
   save() {
@@ -86,43 +94,58 @@ export class TodoModalComponent implements OnInit {
     this.movimento.sottocategoria = this.sottocategoria;
     this.movimento.luogo = this.luogo;
     this.movimento.saldoDaRipartire = this.saldoDaRipartire;
-   
-    var d = new Date(this.date);
-    var n = d.getTime();
+
+    const d = new Date(this.date);
+    const n = d.getTime();
     this.movimento.data = n;
 
-    console.log("Movimento: ", this.movimento);
-    var res = this.addCredito(this.movimento)
-    //res;
-    this.presentToast();
-    setTimeout(() => { this.modalController.dismiss() }, 2000);
+    console.debug('Movimento: ', this.movimento);
+    const res = this.addCredito(this.movimento);
+    // res;
+    this.presentToast('success',1500,'Your settings have been saved.');
+    setTimeout(() => { this.modalController.dismiss(); }, 2000);
   }
 
-  changeValue(obj){
-   
-    if(this.saldoDaRipartire > this.valore){
-      console.log("This value: ", this.valore);
-      console.log("This saldoDaRip: " , this.saldoDaRipartire);  
-      console.log("Azzera saldo da ripartire")
-      this.saldoDaRipartire == 0;
+  refreshVisibility() {
+    console.debug("IDFONDO :", this.idFondo);
+    if( this.idFondo === undefined || this.idFondo == "null" ){
+      this.saldoBooleanTrue = true;
+    } else{
+      this.saldoBooleanTrue = false;
     }
-    console.log("Obj :" , obj);
   }
 
+  refreshVisibilityFromValue(){
+    console.debug("valore :", this.valore);
+    console.debug("saldoDaRipartire :", this.saldoDaRipartire);
+    if (this.valore && (this.saldoDaRipartire > this.valore) ){
+      this.presentToast('danger',2000,'Saldo da ripartire maggiore del valore inserite');
+      this.saldoDaRipartire = null;
+      this.saldoErrorClass = true;
+    } else {
+      if(this.saldoDaRipartire != null){
+      this.saldoErrorClass = false;
+      }
+    }
+  }
 
-  async presentToast() {
-    //posso fare il toast con l'errore impostando il colore del toast
-    //danger , dark , success
-    //con l'errore che facciamo? rimaniamo un pò di più il toast?
-    var colorToast = 'success';
-    var durationToast = 1500;
+  async presentToast(color,duration,message) {
+    // posso fare il toast con l'errore impostando il colore del toast
+    // danger , dark , success
+    // con l'errore che facciamo? rimaniamo un pò di più il toast?
+    const colorToast = color ? color : 'warning';
+    const durationToast = duration ? duration : 1200;
+    const messageToast = message ? message : "Generic Error";
     const toast = await this.toastController.create({
-      message: 'Your settings have been saved.',
+      message: messageToast,
       duration: durationToast,
       color: colorToast
     });
     toast.present();
   }
 
- 
+
+  logElements(obj){
+    console.debug(obj);
+  }
 }
